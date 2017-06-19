abac85809007e8f4e28c4188500112f478a3956b
Magicfar4 <vany.58.98.2013@gmail.com>
2017-06-19 20:44:36 +0300

    Reload Css logic and add js logic

--- assets/js/jm_header.js
+++ assets/js/jm_header.js
@@ -2,32 +2,16 @@
  * Created by Magicmen on 19.06.2017.
  */
 
-var jm_header = {
-    header_height: $('#header').height(),
-    menu_height: $('#header_menu').height(),
-    position: [
-        'absolute',
-        'bottom'
-    ],
-
-    move_to_up: function (element_id) {
-        var $opacity = (window.scrollX == 492)?'1':'0.'+(window.scrollX);
-
-        if ((jm_header.header_height - jm_header.menu_height) != 492) {
-            $('#'+element_id).css({
-                "background": 'rgba(255,255,255,'+$opacity+')'
-            });
-        } else {
-            $('#'+element_id).css({
-                "background": 'rgba(255,255,255,1)'
-            });
-        }
-
-    }
-};
-
 $(document).ready(function () {
     $(window).scroll(function () {
-        jm_header.move_to_up($('#header_menu').attr('id'));
+        var navigation = $('#navigation');
+        var object_to_stick = $(navigation).offset().top;
+        var windowScroll = $(window).scrollTop();
+
+        if (windowScroll > object_to_stick) {
+            $(navigation).css("position", "fixed");
+        } else {
+            $(navigation).css("position", "relative");
+        }
     });
 });
