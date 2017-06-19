7f1f9902cb920a156b3491dcbf7c00fc4f6e77f4
Magicfar4 <vany.58.98.2013@gmail.com>
2017-06-19 19:22:40 +0300

    Reload Css logics

---
+++ assets/js/jm_header.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Magicmen on 19.06.2017.
+ */
+
+/*var jm_header = {
+    height: $('#header').height(),
+    position: [
+        'absolute',
+        'bottom'
+    ],
+    
+    move_to_up: function (element_id) {
+        
+    }
+};*/
