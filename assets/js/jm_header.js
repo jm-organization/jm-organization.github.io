89fd09675cb83e6c0b98cfb2bac0da495b9f2f64
Magicfar4 <vany.58.98.2013@gmail.com>
2017-06-19 21:18:04 +0300

    JM-HEADER::remake

--- assets/js/jm_header.js
+++ assets/js/jm_header.js
@@ -3,11 +3,10 @@
  */
 
 $(document).ready(function () {
-    var window = $(window),
-        opacityControl = $('.jm-navigation-bar');
+    var opacityControl = $('.jm-navigation-bar');
 
-    window.scroll(function(){
-        var top = window.scrollTop(),
+    $(window).scroll(function(){
+        var top = $(window).scrollTop(),
             opacity = top > 500 ? 1 : top * 2 / 1000;
 
         opacityControl.css('opacity', opacity);
