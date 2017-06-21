29bb2f7c4dc85a534c5229b56c743f33519688e1
Magicfar4 <vany.58.98.2013@gmail.com>
2017-06-21 20:21:57 +0300

    Изменение функционала странички

--- assets/js/jm_header.js
+++ assets/js/jm_header.js
@@ -4,24 +4,23 @@
 
 $(document).ready(function () {
     var opacityControl = $('.jm-layer');
+    var top = $(window).scrollTop(),
+        opacity = top > 500 ? 1 : top * 2 / 950;
 
     $(window).scroll(function(){
-        var top = $(window).scrollTop(),
-            opacity = top > 500 ? 1 : top * 2 / 950;
-
         opacityControl.css('opacity', opacity);
         if (top > 3 ) {
             $('.jm-layer').css('z-index', '6');
         } else {
             $('.jm-layer').css('z-index', '');
         }
-
-        if (top == 0) {
-            $('.jm-warp.warp-button-to-top').css({'z-index':'-1', 'opacity':'0'});
-        } else if (top > window.screen.availHeight) {
-            $('.jm-warp.warp-button-to-top').css({'z-index':'1', 'opacity':'1'});
-        }
     });
+
+    if (top == 0) {
+        $('.jm-warp.warp-button-to-top').css({'z-index':'-1', 'opacity':'0'});
+    } else if (top > window.screen.availHeight) {
+        $('.jm-warp.warp-button-to-top').css({'z-index':'1', 'opacity':'1'});
+    }
 
     $('.warp-button-to-content').on('click', 'a', function () {
         if ($(this).data('href') == '@content') {
