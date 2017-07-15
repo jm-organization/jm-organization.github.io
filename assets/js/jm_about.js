bac1505ec85ddb66d0185e2283481a439d19597b
Magicfar4 <vany.58.98.2013@gmail.com>
2017-07-15 12:34:48 +0300

    Обновлён список учасников

---
+++ assets/js/jm_about.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Admin on 15.07.2017.
+ */
+
+var web_sites = {
+    ws: $('#web-sites')
+};
+
+$(document).ready(function () {
+    var $sites = web_sites.ws.split(',');
+
+    $sites.forEach(function (site) {
+        web_sites.ws.append(tags.tag({
+            tag: 'span',
+            attributes: {
+                class: 'label label-success'
+            },
+            tagContent: site
+        }));
+    });
+});
