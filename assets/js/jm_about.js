ba572739edc76556db2c85cf07e022db445fb8df
Magicfar4 <vany.58.98.2013@gmail.com>
2017-07-15 12:55:02 +0300

    fixes

--- assets/js/jm_about.js
+++ assets/js/jm_about.js
@@ -4,13 +4,14 @@
 
 $(document).ready(function () {
     var web_sites = {
-        ws: $('#web-sites').data('ws')
+        ws_block: $('#web-sites'),
+        ws: web_sites.ws_block.data('ws')
     };
 
     var $sites = web_sites.ws.split(',');
 
     $sites.forEach(function (site) {
-        web_sites.ws.append(tags.tag({
+        web_sites.ws_block.append(tags.tag({
             tag: 'span',
             attributes: {
                 class: 'label label-success'
