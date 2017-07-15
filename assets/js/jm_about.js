76c69d9cc4e644d53bd0d8eab6950cf7dd9006d6
Magicfar4 <vany.58.98.2013@gmail.com>
2017-07-15 13:06:24 +0300

    fixes

--- assets/js/jm_about.js
+++ assets/js/jm_about.js
@@ -4,16 +4,25 @@
 
 $(document).ready(function () {
     var web_sites_block = $('#web-sites');
+    var href_pattern_before = /([a-z]{3,6}:\/\/)/g;
+    var href_pattern_after = /\/([\w@\/?=&\.%#]{0,256})\/?/g;
 
     var $sites = web_sites_block.data('ws').split(',');
 
     $sites.forEach(function (site) {
         web_sites_block.append(tags.tag({
-            tag: 'span',
+            tag: 'a',
+            tagContent: tags.tag({
+                tag: 'span',
+                attributes: {
+                    class: 'label label-success',
+                    style: 'padding-right: 4px'
+                },
+                tagContent: site
+            }),
             attributes: {
-                class: 'label label-success'
-            },
-            tagContent: site
+                href: site.replace(href_pattern_before, '').replace(href_pattern_after, '')
+            }
         }));
     });
 });
