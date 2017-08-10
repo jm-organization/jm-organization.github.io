d1f0b47ff1df0a6b126819aa0f5df6b373eaedc6
Magicfar4 <vany.58.98.2013@gmail.com>
2017-08-10 16:56:48 +0300

    fixes

--- assets/js/jm_search.js
+++ assets/js/jm_search.js
@@ -4,7 +4,7 @@
 
 function search_news(news, filter) {
     $('#article').each(function () {
-        if ($(this).data('article').search(news) > 0) {
+        if ($(this).data('article').search(news) >= 0) {
             $(this).show();
         } else {
             $(this).hide();
