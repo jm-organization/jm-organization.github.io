429e3598c634d4c82c7546864165b949e0134fa4
Magicfar4 <vany.58.98.2013@gmail.com>
2017-08-10 12:20:08 +0300

    fixes

---
+++ assets/js/jm_search.js
@@ -0,0 +1,15 @@
+/**
+ * Created by JM Organization on 10.08.2017.
+ */
+
+var input = $('#search');
+
+input.oninput = function() {
+    $('#article').each(function () {
+        
+    })
+};
+
+input.onclick = function () {
+    $('.jm-news-found').show();
+};
