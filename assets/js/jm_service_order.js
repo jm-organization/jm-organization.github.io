e92dc9d6f21554264c4e7d0e5ff5900c8d26f7a3
Magicfar4 <vany.58.98.2013@gmail.com>
2017-08-13 21:34:29 +0300

    fixes

--- assets/js/jm_service_order.js
+++ assets/js/jm_service_order.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     $('[data-open="order-modale"]').click(function (element) {
         var subject = "#order-subject-hidden";
         var subject_view = "#order-subject-text";
-        var subject_text = $(subject)[0].value+' '+element.target.dataset.service;
+        var subject_text = '[Заказ] '+element.target.dataset.service;
 
         $('#order-modale').modal('show');
         $(subject).val(subject_text);
