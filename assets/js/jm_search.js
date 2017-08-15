bf4140f0cdfd1f0eefb57dfbd0996205f62dc208
Magicfar4 <vany.58.98.2013@gmail.com>
2017-08-15 17:30:50 +0300

    fixes

--- assets/js/jm_search.js
+++ assets/js/jm_search.js
@@ -3,41 +3,21 @@
  */
 
 $(document).ready(function () {
+    var articles_list = $("#articles").data("articles"),
+        articles_list_array = articles_list.substring(0, articles_list.length - 1).split('/');
+
     $(document).on('input', '#search', function () {
         var $item = $(this),
             value = $item.val(),
-            pattern = new RegExp(value, "i"),
-            articles_list = $("#articles").data("articles"),
-            articles_list_array = articles_list.substring(0, articles_list.length - 1).split('/');
+            pattern = new RegExp(value, "i");
 
-        articles_list_array.each(function () {
-            var article_title = this.title,
-                article_id = this.id;
-
-            if (pattern.test(article_title)) {
-                $("#article-"+article_id).show();
+        articles_list_array.each(function (index, title) {
+            if (pattern.test(title)) {
+                $("#article-"+index).show();
             } else if (news != '' && news != ' ') {
                 $('.short-article').hide();
             }
         });
     });
 });
-
-function search_news(news, filter) {
-    var pattern = new RegExp(news, "i");
-
-    if (news != '' && news != ' ') {
-        $('.short-article').each(function () {
-            var title = $(this).data('article');
-
-            if (pattern.test(title)) {
-                $(this).show();
-            } else if (news != '' && news != ' ') {
-                $('.short-article').hide();
-            }
-
-            console.log(pattern.test(title) + " / title: "+ title + " / pattern: "+ pattern)
-        });
-    }
-}
 
