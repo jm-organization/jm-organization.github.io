cb298227fa4df59fa27d0856a3fd49bbe1961267
Magicfar4 <vany.58.98.2013@gmail.com>
2017-08-15 15:53:26 +0300

    fixes

--- assets/js/jm_search.js
+++ assets/js/jm_search.js
@@ -2,6 +2,27 @@
  * Created by JM Organization on 10.08.2017.
  */
 
+$(document).ready(function () {
+    var articles_list = $("#articles").data("articles");
+
+    $(document).on('input', '#search', function () {
+        var $item = $(this),
+            value = $item.val(),
+            pattern = new RegExp(value, "i");
+
+        articles_list.each(function () {
+            var article_title = this.title,
+                article_id = this.id;
+
+            if (pattern.test(article_title)) {
+                $("#article-"+article_id).show();
+            } else if (news != '' && news != ' ') {
+                $('.short-article').hide();
+            }
+        });
+    });
+});
+
 function search_news(news, filter) {
     var pattern = new RegExp(news, "i");
 
@@ -20,23 +41,3 @@ function search_news(news, filter) {
     }
 }
 
-$(document).ready(function () {
-    var articles_list = $("#articles").data("articles");
-    
-    $(document).on('input', '#search', function () {
-        var $item = $(this),
-            value = $item.val(),
-            pattern = new RegExp(value, "i");
-
-            articles_list.each(function () {
-                var article_title = this.title,
-                    article_id = this.id;
-
-                if (pattern.test(article_title)) {
-                    $("#article-"+article_id).show();
-                } else if (news != '' && news != ' ') {
-                    $('.short-article').hide();
-                }
-            });
-    }); 
-});
