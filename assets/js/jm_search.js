57cd3d626b24c469ab04dc0a08652450daaeaf9e
Magicfar4 <vany.58.98.2013@gmail.com>
2017-08-15 19:16:24 +0300

    fixes

--- assets/js/jm_search.js
+++ assets/js/jm_search.js
@@ -2,16 +2,20 @@
  * Created by JM Organization on 10.08.2017.
  */
 
-$(document).ready(function () {
-    var articles_list = $("#articles").data("articles"),
-        articles_list_array = articles_list.substring(0, articles_list.length - 1).split('/');
+var articles = {
+    list_id: '#articles_list',
 
+    list: $(articles.list_id).data('articles'),
+    articles_list_array: articles.list.substring(0, articles.list.length - 1).split('/')
+};
+
+$(document).ready(function () {
     $(document).on('input', '#search', function () {
         var $item = $(this),
             value = $item.val(),
             pattern = new RegExp(value, "i");
 
-        $("#articles").data("articles").substring(0, $("#articles").data("articles").length - 1).split('/').each(function (index, title) {
+        articles.articles_list_array.each(function (index, title) {
             if (pattern.test(title)) {
                 $("#article-"+index).show();
             } else if (news != '' && news != ' ') {
